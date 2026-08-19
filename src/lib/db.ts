e37116a8e62932570a1a45f2b00ae76bd93f5d7f
yashsoloman7 <yashsoloman7@users.noreply.github.com>
2026-08-19 17:45:57 +0530

    updated recently

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,10 +2,6 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
 /**
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections growing exponentially
@@ -27,6 +23,10 @@ if (!cached) {
 }
 
 async function dbConnect() {
+    if (!MONGODB_URI) {
+        throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+    }
+
     if (cached.conn) {
         return cached.conn;
     }
