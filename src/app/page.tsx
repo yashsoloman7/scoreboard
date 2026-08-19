b64a90f473801bbb4a951ce9fbc3a8e0347353cf
yashsoloman7 <yashsoloman7@users.noreply.github.com>
2026-08-19 18:56:11 +0530

    updated recently

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,16 @@
-﻿import { cookies } from 'next/headers'
-import { createServerClient } from '@supabase/ssr'
-import { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies'
+﻿import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
 
 export default async function Page() {
   const cookieStore = await cookies()
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll()
-        },
-        setAll(cookiesToSet: { name: string; value: string; options?: Partial<ResponseCookie> }[]) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {
-            // The `setAll` method was called from a Server Component.
-          }
-        },
-      },
-    }
-  )
+  const supabase = createClient(cookieStore)
 
   const { data: todos } = await supabase.from('todos').select()
 
   return (
     <ul>
       {todos?.map((todo: any) => (
-        <li key={todo.id}>{todo.title}</li>
+        <li key={todo.id}>{todo.name}</li>
       ))}
     </ul>
   )
